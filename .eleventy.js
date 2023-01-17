ab96b11b66f9923eccc1ca407174fd421017da7d
paulrobertpine <paulrobertpine@gmail.com>
2023-01-17 14:58:49 -0500

    working on external link icon

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
+// see https://dev.to/giulia_chiola/add-html-classes-to-11ty-markdown-content-18ic
+const markdownIt = require("markdown-it")
+const markdownItAttrs = require("markdown-it-attrs")
+
+const markdownItOptions = {
+  html: true,
+  breaks: true,
+  linkify: true,
+}
+
+const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
+
 module.exports = function (config) {
+  config.setLibrary("md", markdownLib)
   config.addWatchTarget("./src/sass")
   config.addPassthroughCopy("./src/css")
   config.addPassthroughCopy("./src/img")
